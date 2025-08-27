b8f49553f3b6345fa5af407081bc9ef152ce2e31
2zm00 <dwa010301@gmail.com>
2025-08-27 16:22:44 +0900

    feat: add welcome view

---
+++ practice/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>React Page</title>
+    @viteReactRefresh
+    @vite('resources/js/app.tsx') <!-- React 진입점 -->
+</head>
+<body>
+    <div id="app"></div> <!-- React 컴포넌트가 여기 렌더링됨 -->
+</body>
+</html>
